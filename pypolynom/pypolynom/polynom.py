c15a1c7e74f7988c272e585649810ea9bea61605
payno <henri.payno@esrf.fr>
2019-04-01 19:35:45 +0200

    Add polynom file

---
+++ pypolynom/pypolynom/polynom.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+"""This is a simple demonstration library"""
+
+__authors__ = ["Pierre Knobel", "Jerome Kieffer", "Pierre Palero",
+               "Henri Payno", "Armando Sole", "Valentin Valls",
+               "Thomas Vincent"]
+__date__ = "01/04/2019"
+__license__ = "MIT"
+
+
+from . import mathutil
+
+
+def polynom(a, b, c):
+    """Solve the polygon of order two.
+
+    .. math:: a\cdotx^2 + b\cdotx + c = 0
+
+    :param float a: a value of the polynom
+    :param float b: b value of the polynom
+    :param float c: c value of the polynom
+    :rtype: List[float]
+    """
+    if a == 0:
+        # Not a polynom
+        raise ValueError("Not a quadratic equation (a==0)")
+    delta = mathutil.pow2(b) - 4.0 * a * c
+    solutions = []
+    if delta > 0:
+        solutions.append((-b + mathutil.sqrt(delta)) / (2.0 * a))
+        solutions.append((-b - mathutil.sqrt(delta)) / (2.0 * a))
+    elif delta == 0:
+        solutions.append(-b/(2.0*a))
+    return solutions
