17ea8e734656e8dab70e891e159e54fd3d73de2f
payno <henri.payno@esrf.fr>
2019-04-01 19:33:05 +0200

    Add mathutil.py

---
+++ mathutil.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+"""This is a simple demonstration library"""
+
+__authors__ = ["Pierre Knobel", "Jerome Kieffer", "Pierre Palero",
+               "Henri Payno", "Armando Sole", "Valentin Valls",
+               "Thomas Vincent"]
+__date__ = "01/04/2019"
+__license__ = "MIT"
+
+
+def sqrt(x):
+    return x**0.5
+
+
+def pow2(x):
+    return x*x
